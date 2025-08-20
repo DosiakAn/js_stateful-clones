03026b85719cdc3b4a550f59524b68763c579a32
Anastasiia <anastasiia.dosiak@gmail.com>
2025-08-20 13:12:06 +0300

    add solutions

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,25 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  let modifyState = { ...state };
+  const newArray = [];
+
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      modifyState = { ...modifyState, ...action.extraData };
+    } else if (action.type === 'removeProperties') {
+      modifyState = { ...modifyState };
+
+      for (const key of action.keysToRemove) {
+        delete modifyState[key];
+      }
+    } else if (action.type === 'clear') {
+      modifyState = {};
+    }
+    newArray.push({ ...modifyState });
+  }
+
+  return newArray;
 }
 
 module.exports = transformStateWithClones;
